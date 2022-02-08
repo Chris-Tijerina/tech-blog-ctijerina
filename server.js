76c45d84cc01d0af038c23e5d09e87dceb12bd79
Christopher <ctijerina@live.com>
2022-02-07 20:55:21 -0600

    added schema file

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 sequelize.sync({ force: false }).then(() => {
-	app.listen(PORT, () => console.log("Now listening"));
+	app.listen(PORT, () => console.log("Now listening on Port 3001!"));
 });
