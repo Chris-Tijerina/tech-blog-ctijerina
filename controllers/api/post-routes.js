c3ab3f8cbd2401e2bbc7c5ffcac61e5a9856e6f2
Christopher <ctijerina@live.com>
2022-02-13 15:35:24 -0600

    added a dashboard and the ability to edit posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Post, User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // get all users
 router.get("/", (req, res) => {
@@ -46,12 +47,12 @@ router.get("/:id", (req, res) => {
 		});
 });
 
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
 	// expects {title: 'Taskmaster goes public!', post_text: 'https://taskmaster.com/press', user_id: 1}
 	Post.create({
 		title: req.body.title,
 		post_text: req.body.post_text,
-		user_id: req.body.user_id,
+		user_id: req.session.user_id,
 	})
 		.then((dbPostData) => res.json(dbPostData))
 		.catch((err) => {
@@ -60,7 +61,7 @@ router.post("/", (req, res) => {
 		});
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
 	Post.update(
 		{
 			title: req.body.title,
@@ -85,7 +86,7 @@ router.put("/:id", (req, res) => {
 		});
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
 	Post.destroy({
 		where: {
 			id: req.params.id,
